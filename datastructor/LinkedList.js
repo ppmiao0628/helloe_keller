1dc7925b9458269b252f3991933c0cdef35db6fd
Keller Me <kellerme@KellerdeMBP.lan>
2018-06-02 09:17:06 +0800

    append和removeAt

--- datastructor/LinkedList.js
+++ datastructor/LinkedList.js
@@ -6,13 +6,39 @@ function LinkedList() {
     let length = 0;
     let head = null;
     this.append = function (element) {
-
+        let node = new Node(element);
+        let current = null;
+        if (!head) {
+            head = node;
+        } else {
+            current = head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        length++;
     };
     this.insert = function (position, element) {
 
     };
     this.removeAt = function (position) {
-
+        if (position > -1 && position < length) {
+            let current = head;
+            let previous = null;
+            let index = 0;
+            if (position === 0) {
+                head = current.next;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+            }
+            length--;
+            return current.element;
+        }
     };
     this.remove = function (element) {
 
@@ -24,7 +50,7 @@ function LinkedList() {
 
     };
     this.size = function () {
-
+        return length;
     };
     this.getHead = function () {
 
@@ -36,3 +62,12 @@ function LinkedList() {
 
     };
 }
+
+//test
+let list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+console.log(list.removeAt(1));
+console.log(list.size());
