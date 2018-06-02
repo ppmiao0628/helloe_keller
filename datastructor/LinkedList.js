2019828eacba3fe285ae658b0557cd97d9439974
Keller Me <kellerme@KellerdeMBP.lan>
2018-06-02 09:33:10 +0800

    其他方法补全

--- datastructor/LinkedList.js
+++ datastructor/LinkedList.js
@@ -20,7 +20,27 @@ function LinkedList() {
         length++;
     };
     this.insert = function (position, element) {
-
+        if (position > -1 && position < length) {
+            let node = new Node(element);
+            let current = head;
+            let previous = null;
+            let index = 0;
+            if (position === 0) {
+                node.next = current;
+                head = node;
+            } else {
+                while (index++ < position) {
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            length++;
+            return true;
+        } else {
+            return false;
+        }
     };
     this.removeAt = function (position) {
         if (position > -1 && position < length) {
@@ -41,25 +61,41 @@ function LinkedList() {
         }
     };
     this.remove = function (element) {
-
+        let index = this.indexOf(element);
+        return this.removeAt(index);
     };
     this.indexOf = function (element) {
-
+        let current = head;
+        let index = 0;
+        while (current) {
+            if (element === current.element) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
     };
     this.isEmpty = function () {
-
+        return length === 0;
     };
     this.size = function () {
         return length;
     };
     this.getHead = function () {
-
+        return head;
     };
     this.toString = function () {
-
+        let current = head;
+        let string = '';
+        while (current) {
+            string += current.element + (current.next ? ',' : '');
+            current = current.next;
+        }
+        return string;
     };
     this.print = function () {
-
+        return this.toString();
     };
 }
 
@@ -69,5 +105,11 @@ list.append(1);
 list.append(2);
 list.append(3);
 list.append(4);
-console.log(list.removeAt(1));
-console.log(list.size());
+// console.log(list.removeAt(1));
+console.log(list.insert(1, 8));
+// console.log(list.size());
+console.log(list.indexOf(0));
+console.log(list.indexOf(3));
+console.log(list.toString());
+console.log(list.remove(2));
+console.log(list.toString());
